50706153513dfdbeb0816e0612bf4412bc3f0919
ZALFA\zalfadu <zdhiyaulhaq7@gmail.com>
2023-05-10 00:23:26 +0800

    lpk

---
+++ chemspace.py
@@ -0,0 +1,138 @@
+import streamlit as st
+st.sidebar.title('Menu')
+with st.sidebar:
+        option = st.radio('Pilih salah satu:',['INTRO','KALKON','KANGEN','DEBARAN'])
+if option == "INTRO": 
+    st.image("https://i.pinimg.com/originals/6d/32/d9/6d32d95c7f04f8870a4980bd538bc3e4.gif")
+    st.header(':blue[CHEMISTRY SPACE🌌]')
+    st.write('*Website khusus perhitungan untuk bidang Analisis Kimia dan terapannya*')
+    st.write('Dikembangkan oleh Kelompok 7 ACETYLCHOLINE')
+
+if option == "KALKON":
+    st.header(':red[Welcome to KALKON!] 🤭')
+    st.subheader('Kalkulator Konsentrasi')
+    #Input slide
+    progress_text = "Operation in progress. Please wait."
+
+    #Khusus titrimetri
+    st.write(":blue[*Perhitungan Konsentrasi Titran Khusus Titrimetri*]")
+    #Input volume khusus titrimetri
+    number1 = st.number_input('Masukkan Volume Titran (mL)')
+    st.write(f'Volume Titran adalah {number1}')
+    #Input bobot sampel
+    number2 = st.number_input('Masukkan Bobot Sampel (mg) atau (kg)')
+    st.write(f'Bobot Sampel adalah {number2}')
+    #Input fp
+    number3 = st.number_input('Masukkan Faktor Pengencer')
+    st.write(f'Faktor Pengencer adalah {number3}')
+    #Input Berat Ekivalen
+    number4 = st.number_input('Masukkan Berat Ekivalen (mg/mgrek) atau Masukan Berat Molekul (mg/mmol)')
+    st.write(f'Berat Ekivalen adalah {number4}')
+    if st.button('Hitung Normalitas'):
+        Normalitas = number2/(number1*number3*number4)
+        st.success(f'Normalitas adalah {Normalitas}N')
+    if st.button('Hitung Molaritas'):
+        Molaritas = number2/(number1*number3*number4)
+        st.success(f'Molaritas adalah {Molaritas}M')
+    else:
+        st.write('Silahkan tekan tombol hitung😊')
+        
+    #Khusus Kadar b/v atau b/b
+    st.write(':blue[*Perhitungan Konsentrasi Kadar Sampel %(b/b), %(b/v), atau ppm Khusus Titrimetri*]')
+    #Input Normalitas Titran
+    number15 =  st.number_input(
+        'Normalitas (mgrek/ml) atau Masukan Molaritas (mmol/ml) dari Titran',
+         step=1e-6,
+        format="%.4f")
+    st.write(f'Normalitas atau Molaritas adalah {number15}')
+    #Input volume titran
+    number16 = st.number_input('Volume Titran (mL)')
+    st.write(f'Volume Titran adalah {number16}')
+    #Input Volume titrat
+    number17 =  st.number_input('Volume titrat/sampel (mL) atau (L)')
+    st.write(f'Volume titrat/sampel adalah {number17}')
+    #Input bobot sampel
+    number18 = st.number_input('Berat sampel (mg) atau (kg)')
+    st.write(f'Berat sampel adalam {number18}')
+    #Input Berat Ekivalen
+    number19 = st.number_input('Berat Ekivalen (mg/mgrek) atau Masukan Berat Molekul (mg/mmol)')
+    st.write(f'Berat Ekivalen adalah {number19}')
+    #Input 10
+    number20 = 1/1000
+    #Input fp
+    number21 = st.number_input('Faktor Pengencer')
+    st.write(f'Faktor Pengencer adalah {number21}')
+    #Input Persen 
+    number22 = 100 
+    if st.button('Hitung Kadar % (b/v)'):
+        Kadar = number15*number16*number19*number20*number21*number22/number17
+        st.success(f'Kadar sebesar {Kadar}%(b/v)')
+    if st.button('Hitung Kadar % (b/b)'):
+        Kadar = number15*number16*number19*number20*number21*number22/number18
+        st.success(f'Kadar sebesar {Kadar}%(b/b)')   
+    if st.button('Hitung PPM'):
+        ppm = number15*number16*number19/number17
+        st.success(f'ppm sebesar {ppm}ppm')
+    else:
+        st.write('Silahkan tekan tombol hitung😊')
+        
+if option == "KANGEN":
+    #Write
+    st.header(':orange[This is KANGEN!]🥰')
+    st.subheader('Kalkulator Pengenceran')
+    #Input slide
+    progress_text = "Operation in progress. Please wait."
+    #Input Konsentrasi
+    number1 = st.number_input ('Masukkan Konsentrasi Sebelum Pengenceran')
+    number2 = st.number_input ('Masukkan Konsentrasi Sesudah Pengenceran')
+    #Input Volume
+    number3 = st.number_input ('Masukkan Volume Sebelum Pengenceran (mL)')
+    number4 = st.number_input ('Masukkan Volume Sesudah Pengenceran (mL)')
+    if st.button ('Konsentrasi yang dicari adalah'):
+        Konsentrasi = (number2*number4)/number3
+        Konsentrasi = (number1*number3)/number4
+        st.success(f'Konsentrasinya sebesar {Konsentrasi}')
+    if st.button ('Volume yang dicari adalah') :
+        Volume = (number2*number4)/number1
+        Volume = (number1*number3)/number2
+        st.success(f'Volumenya sebesar {Volume}')
+    else :
+        st.write ('Silahkan tekan tombol hitung 😊')
+
+if option == "DEBARAN": 
+    #Write
+    st.header(':green[Deteksi Bahaya Paparan Bahan Kimia]🧪')
+    st.subheader('Mendeteksi tingkat aman ketika bekerja dengan bahan kimia berbahaya')
+    #Input slide
+    progress_text = "Operation in progress. Please wait."
+    #Input nama bahan kimia
+    nama= st.text_input('Masukan bahan kimia yang digunakan')
+    st.write(f'Nama bahan kimia yang anda gunakan adalah {nama}')
+    #Input durasi kerja/hari 
+    durasi1 = st.number_input ('Masukkan durasi kerja/hari (jam)')
+    #Input NAB-TLV-TWA senyawa
+    ppm1 = st.number_input ('Masukkan batas ambang berdasarkan NAB-TLV-TWA (ppm)')
+    #Input durasi paparan 
+    st.caption('Waktu paparan pertama')
+    ppm2 = st.number_input ('Masukkan konsentrasi senyawa yang terhirup pertama (ppm)')
+    durasi2 = st.number_input ('Masukkan durasi terpapar pertama (jam)')
+    st.caption('Waktu paparan kedua')
+    ppm3 = st.number_input ('Masukkan konsentrasi senyawa yang terhirup kedua (ppm)')
+    durasi3 = st.number_input ('Masukkan durasi terpapar kedua (jam)')
+    st.caption('Waktu paparan ketiga')
+    ppm4 = st.number_input ('Masukkan konsentrasi senyawa yang terhirup ketiga (ppm)')
+    durasi4 = st.number_input ('Masukkan durasi terpapar ketiga (jam)')
+    if st.button ('Konsentrasi senyawa yang terhirup/jam bekerja adalah'):
+        konsentrasi = ((ppm2*durasi2) + (ppm3*durasi3) + (ppm4*durasi4))/(durasi2 + durasi3 + durasi4)
+        st.success(f'Konsentrasi senyawa {nama} yang terhirup adalah {konsentrasi}ppm')
+    if st.button ('Konsentrasi senyawa yang terhirup selama satu hari berkerja adalah'):
+        deteksi = (durasi2 + durasi3 + durasi4)/durasi1*((ppm2*durasi2) + (ppm3*durasi3) + (ppm4*durasi4))/(durasi2 + durasi3 + durasi4)
+        st.success(f'Konsentrasi senyawa {nama} yang terhirup selama satu hari bekerja adalah {deteksi}ppm')
+    if st.button("Hasil deteksi"):
+        hasil =  (durasi2 + durasi3 + durasi4)/durasi1*((ppm2*durasi2) + (ppm3*durasi3) + (ppm4*durasi4))/(durasi2 + durasi3 + durasi4)
+        if hasil >= ppm1:
+           st.write("TIDAK AMAN😩")
+        elif hasil <=ppm1:
+           st.write("AMAN😍")
+
+
